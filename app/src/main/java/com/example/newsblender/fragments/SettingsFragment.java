a1ee5aa327ccdc6eeba7b17d7a34a92faa28c6d8
Viacheslav <bkd.slava@gmail.com>
2022-05-05 13:34:58 +0300

    Method to change locale, maybe Deprecated.

--- app/src/main/java/com/example/newsblender/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/newsblender/fragments/SettingsFragment.java
@@ -1,10 +1,15 @@
 package com.example.newsblender.fragments;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.newsblender.R;
+
+import java.util.Locale;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -12,4 +17,21 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
     }
+
+//    private void onClick() {
+//        changeLocale("en");
+//    }
+//
+//    private void changeLocale(String language) {
+//        Resources res = getResources();
+//        DisplayMetrics dm = res.getDisplayMetrics();
+//        Configuration conf = res.getConfiguration();
+//        if (language == "uk") {
+//            conf.setLocale(Locale.getDefault());
+//        }
+//        else {
+//            conf.setLocale(new Locale(language));
+//            res.updateConfiguration(conf, dm);
+//        }
+//    }
 }
