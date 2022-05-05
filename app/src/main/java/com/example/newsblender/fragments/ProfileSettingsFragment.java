14c10bf4b72f53ceff86383b5706d7ed051e7717
kolan4ick <Kolan4ick@gmail.com>
2022-05-06 00:37:39 +0300

    Add ability to change user profile avatar

--- app/src/main/java/com/example/newsblender/fragments/ProfileSettingsFragment.java
+++ app/src/main/java/com/example/newsblender/fragments/ProfileSettingsFragment.java
@@ -1,22 +1,39 @@
 package com.example.newsblender.fragments;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.newsblender.R;
 import com.example.newsblender.classes.ItemViewModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
 import java.util.zip.Inflater;
 
 public class ProfileSettingsFragment extends Fragment {
@@ -25,6 +42,21 @@ public class ProfileSettingsFragment extends Fragment {
     private TextView mEditTextPassword;
     private Button mButtonOk;
     private ItemViewModel mViewModel;
+    // view for image view
+    private ImageView imageView;
+
+    // Uri indicates, where the image will be picked from
+    private Uri filePath;
+
+    // request code
+    private final int PICK_IMAGE_REQUEST = 22;
+
+    // instance for firebase storage and StorageReference
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    FirebaseAuth fAuth;
+    View mView;
+
 
     public static ProfileSettingsFragment newInstance() {
         ProfileSettingsFragment fragment = new ProfileSettingsFragment();
@@ -40,11 +72,11 @@ public class ProfileSettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View mView = inflater.inflate(R.layout.fragment_profile_settings, container, false);
+        mView = inflater.inflate(R.layout.fragment_profile_settings, container, false);
 
         /* Initialize main variables */
         mViewModel = new ViewModelProvider(requireActivity()).get(ItemViewModel.class);
-        FirebaseAuth fAuth = mViewModel.getFAuth();
+        fAuth = mViewModel.getFAuth();
         mEditTextFullName = mView.findViewById(R.id.editTextFullName);
         mEditTextPassword = mView.findViewById(R.id.editTextPassword);
         mEditTextInbox = mView.findViewById(R.id.editTextInbox);
@@ -53,7 +85,6 @@ public class ProfileSettingsFragment extends Fragment {
         /* Set views on screen with current user values */
         mEditTextFullName.setText(fAuth.getCurrentUser().getDisplayName());
         mEditTextInbox.setText(fAuth.getCurrentUser().getEmail());
-
         mButtonOk.setOnClickListener(view -> {
             try {
                 if (!mEditTextInbox.getText().toString().equals(fAuth.getCurrentUser().getEmail())) {
@@ -72,6 +103,135 @@ public class ProfileSettingsFragment extends Fragment {
             }
         });
 
+        // get the Firebase  storage reference
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        imageView = mView.findViewById(R.id.imageViewProfileSettings);
+        StorageReference islandRef = storageReference.child("images/island.jpg");
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        islandRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                // Data for "images/island.jpg" is returns, use this as needed
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+        imageView.setOnClickListener(view -> SelectImage());
+
+        storageReference.child("images/" + Objects.requireNonNull(fAuth.getCurrentUser()).getUid()).getDownloadUrl().addOnSuccessListener(uri -> {
+            Glide.with(this).load(uri).into(imageView);
+        }).addOnFailureListener(uri -> {
+            Toast.makeText(getContext(), "Image not fount, used default!", Toast.LENGTH_SHORT).show();
+        });
         return mView;
     }
+
+    private void SelectImage() {
+
+        // Defining Implicit Intent to mobile gallery
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                PICK_IMAGE_REQUEST);
+    }
+
+    // Override onActivityResult method
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode,
+                                 Intent data) {
+
+        super.onActivityResult(requestCode,
+                resultCode,
+                data);
+
+        // checking request code and result code
+        // if request code is PICK_IMAGE_REQUEST and
+        // resultCode is RESULT_OK
+        // then set image in the image view
+        if (requestCode == PICK_IMAGE_REQUEST
+                && data != null
+                && data.getData() != null) {
+
+            // Get the Uri of data
+            filePath = data.getData();
+            try {
+                // Setting image on image view using Bitmap
+                Bitmap bitmap = MediaStore
+                        .Images
+                        .Media
+                        .getBitmap(mView.getContext().getContentResolver(),
+                                filePath);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                // Log the exception
+                e.printStackTrace();
+            }
+        }
+        uploadImage();
+    }
+
+    // UploadImage method
+    private void uploadImage() {
+        if (filePath != null) {
+
+            // Code for showing progressDialog while uploading
+            ProgressDialog progressDialog
+                    = new ProgressDialog(mView.getContext());
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            // Defining the child of storageReference
+            StorageReference ref
+                    = storageReference
+                    .child("images/" + Objects.requireNonNull(fAuth.getCurrentUser()).getUid());
+
+            // adding listeners on upload
+            // or failure of image
+            // Progress Listener for loading
+            // percentage on the dialog box
+            ref.putFile(filePath)
+                    .addOnSuccessListener(
+                            taskSnapshot -> {
+
+                                // Image uploaded successfully
+                                // Dismiss dialog
+                                progressDialog.dismiss();
+                                Toast
+                                        .makeText(mView.getContext(), "Image Uploaded!!", Toast.LENGTH_SHORT)
+                                        .show();
+                            })
+
+                    .addOnFailureListener(e -> {
+
+                        // Error, Image not uploaded
+                        progressDialog.dismiss();
+                        Toast
+                                .makeText(mView.getContext(),
+                                        "Failed " + e.getMessage(),
+                                        Toast.LENGTH_SHORT)
+                                .show();
+                    })
+                    .addOnProgressListener(
+                            taskSnapshot -> {
+                                double progress
+                                        = (100.0
+                                        * taskSnapshot.getBytesTransferred()
+                                        / taskSnapshot.getTotalByteCount());
+                                progressDialog.setMessage(
+                                        "Uploaded "
+                                                + (int) progress + "%");
+                            });
+        }
+    }
+
 }
