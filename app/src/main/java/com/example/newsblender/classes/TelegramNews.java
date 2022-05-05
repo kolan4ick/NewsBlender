dc50f56f750248dce2074df08de7fbb082e423fc
kolan4ick <Kolan4ick@gmail.com>
2022-05-05 16:31:43 +0300

    Add button details to news, add ability to share news

--- app/src/main/java/com/example/newsblender/classes/TelegramNews.java
+++ app/src/main/java/com/example/newsblender/classes/TelegramNews.java
@@ -1,18 +1,31 @@
 package com.example.newsblender.classes;
 
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.text.Html;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.newsblender.MainActivity;
 import com.example.newsblender.R;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
@@ -51,34 +64,25 @@ public class TelegramNews {
     }
 
     public ConstraintLayout getNews(Context context) {
-        ConstraintLayout constraintLayout = new ConstraintLayout(context);
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(30, 20, 30, 0);
-        constraintLayout.setLayoutParams(layoutParams);
-        ConstraintSet set = new ConstraintSet();
+        ConstraintLayout constraintLayout = (ConstraintLayout) LayoutInflater.from(context).inflate(R.layout.test, null);
         constraintLayout.setBackgroundColor(context.getColor(R.color.gray_tint));
 
         /* Initialize ownerName */
-        TextView ownerName = new TextView(context);
+        TextView ownerName = constraintLayout.findViewById(R.id.ownerName);
         ownerName.setText(Html.fromHtml(getOwnerName()));
-        ownerName.setId(View.generateViewId());
 
         /* Initialize body */
-        TextView body = new TextView(context);
+        TextView body = constraintLayout.findViewById(R.id.body);
         body.setText(Html.fromHtml(getBody()));
-        body.setId(View.generateViewId());
 
         /* Initialize date */
-        TextView date = new TextView(context);
+        TextView date = constraintLayout.findViewById(R.id.date);
         date.setText(getDate().toString());
-        date.setId(View.generateViewId());
 
         /* Initialize photos */
-        SliderView sliderView = new SliderView(context);
-        sliderView.setId(View.generateViewId());
+        SliderView sliderView = constraintLayout.findViewById(R.id.imageSliderView);
         if (getPhotoLinks().size() > 0) {
             ImageView imageView = new ImageView(context);
-            imageView.setId(View.generateViewId());
             sliderView.setIndicatorEnabled(true);
             sliderView.setSliderAdapter(new CustomSliderAdapter(context, photoLinks), false);
             sliderView.setIndicatorAnimation(IndicatorAnimationType.SLIDE); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
@@ -88,24 +92,52 @@ public class TelegramNews {
             sliderView.setScrollTimeInSec(3);
             sliderView.setIndicatorRadius(5);
             sliderView.addView(imageView);
+            sliderView.setMinimumHeight(768);
+        } else {
+            constraintLayout.removeView(sliderView);
+            ConstraintSet set = new ConstraintSet();
+            set.clone(constraintLayout);
+            set.connect(body.getId(), ConstraintSet.TOP, ownerName.getId(), ConstraintSet.BOTTOM);
+            set.applyTo(constraintLayout);
         }
 
-        /* Add all views to ConstraintLayout */
-        sliderView.setMinimumHeight(768);
-        constraintLayout.addView(ownerName);
-        Log.println(Log.DEBUG, "Custom", Integer.toString(photoLinks.size()));
-        if (photoLinks.size() > 0)
-            constraintLayout.addView(sliderView);
-        constraintLayout.addView(body);
-        constraintLayout.addView(date);
-        set.clone(constraintLayout);
-        set.connect(ownerName.getId(), ConstraintSet.TOP, constraintLayout.getId(), ConstraintSet.TOP, 60);
-        if (photoLinks.size() > 0)
-            set.connect(sliderView.getId(), ConstraintSet.TOP, ownerName.getId(), ConstraintSet.BOTTOM, 60);
-        set.connect(body.getId(), ConstraintSet.TOP, photoLinks.size() > 0 ? sliderView.getId() : ownerName.getId(), ConstraintSet.BOTTOM, 60);
-        set.connect(date.getId(), ConstraintSet.TOP, body.getId(), ConstraintSet.BOTTOM, 10);
-        set.applyTo(constraintLayout);
+        /* Initialize button */
+        ImageView detailsBtn = constraintLayout.findViewById(R.id.buttonDetails);
+        detailsBtn.setOnClickListener(view -> {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupView = inflater.inflate(R.layout.pop_up_window_news, null);
+            DrawerLayout mDrawerLayout = ((MainActivity) context).findViewById(R.id.drawer_layout);
+            Log.println(Log.DEBUG, "tag", constraintLayout.getParent().getParent().getParent().getParent().getParent().getParent().getParent().toString());
 
+            // create the popup window
+            int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+            int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+            final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+
+            /* When popup is dismissed */
+            popupWindow.setOnDismissListener(() -> {
+                if (mDrawerLayout.getChildAt(mDrawerLayout.getChildCount() - 1).getClass().equals(View.class))
+                    mDrawerLayout.removeViewAt(mDrawerLayout.getChildCount() - 1);
+            });
+
+            popupWindow.showAtLocation(view, Gravity.END | Gravity.TOP, 5, Util.getLocationOnScreen(view).y + view.getHeight() + 15);
+
+            popupView.findViewById(R.id.shareTextView).setOnClickListener(item -> {
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here");
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, "t.me/" + getLinkToNews());
+                context.startActivity(Intent.createChooser(sharingIntent, "Share text via"));
+            });
+
+            popupView.findViewById(R.id.saveTextView).setOnClickListener(item -> {
+                /* TODO: create saving news */
+            });
+
+            View view1 = new View(context);
+            view1.setBackgroundResource(R.drawable.background_tint);
+            mDrawerLayout.addView(view1);
+        });
         return constraintLayout;
     }
 
--- app/src/main/java/com/example/newsblender/classes/Util.java
+++ app/src/main/java/com/example/newsblender/classes/Util.java
@@ -1,12 +1,20 @@
 package com.example.newsblender.classes;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.view.View;
 
 public class Util {
     public static boolean isNetworkAvailable(ConnectivityManager systemService) {
         NetworkInfo activeNetworkInfo = systemService.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public static Point getLocationOnScreen(View view) {
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        return new Point(location[0], location[1]);
     }
 }
